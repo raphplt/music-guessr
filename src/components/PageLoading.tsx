cdbdd833b8369e7cda367e02ae08777a1707e55c
raphplt <raphael.plassart@gmail.com>
2026-09-11 14:59:31 +0200

    Restart project

--- src/components/PageLoading.tsx
+++
@@ -1,7 +0,0 @@
-export function PageLoading() {
-  return (
-    <main className="flex flex-1 items-center justify-center px-4 py-10">
-      <p className="text-zinc-500">Chargement...</p>
-    </main>
-  );
-}
